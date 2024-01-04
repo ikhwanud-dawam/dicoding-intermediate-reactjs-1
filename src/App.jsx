c49c0b1bb2991030212754df55113722bffc6384
Mohammad Ikhwanud Dawam <d4w4m123@gmail.com>
2024-01-04 17:05:21 +0700

    Adding navigation and routes for app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from "react";
+import { Link, Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import NotesWrapper from "./pages/Notes";
 
 function App() {
   return (
     <div className="app-container">
-      <h1>Hello, React</h1>
+      <header>
+        <h1>
+          <Link to={"/"}>Aplikasi Catatan</Link>
+        </h1>
+        <Navigation />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<NotesWrapper />}></Route>
+        </Routes>
+      </main>
     </div>
   );
 }
